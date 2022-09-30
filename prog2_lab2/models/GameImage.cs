d6c2eb100a7e2debbeff328d71c3fc2c0c4bcbcf
Boomrock <63188345+Boomrock@users.noreply.github.com>
2022-10-01 00:47:55 +0700

    Добавьте файлы проекта.

---
+++ prog2_lab2/models/GameImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace prog2_lab2
+{
+    class GameImage : INotifyPropertyChanged
+    {
+        private BitmapImage Image;
+        private bool isСhosen;
+
+        public bool IsСhosen
+        {
+            get
+            {
+                return isСhosen;
+            }
+            set
+            {
+                this.isСhosen = IsСhosen;
+                OnPropertyChanged("isСhosen");
+            }
+        }
+
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        //Метод, который скажет ViewModel, что нужно передать виду новые данные
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+    
+}
